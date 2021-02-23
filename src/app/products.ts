e11cd5cff89fbc19c88b79eef366d7ab5d734d2f
Arujanm001 <arujanm001@gmail.com>
2021-02-23 23:42:57 +0500

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,24 +1,33 @@
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
-    price: 799,
-    description: 'A large phone with one of the best screens'
+    name: "Supreme Grind",
+    price: 59.95,
+    description:
+      "Removable grind chamber holds enough ground coffee for 32 cups.4 to 18-cup slide dial.Scoop/Cleaning Brush is included.18-position grind selector; 4- to 18-cup slide dial"
   },
   {
     id: 2,
-    name: 'Phone Mini',
-    price: 699,
-    description: 'A great phone with one of the best cameras'
+    name: "Seal Glass Pitcher",
+    price: 16.99,
+    description:
+      "Textured glass allows for an immediate view of the beverage contained. Made in Italy.An integrated strong handle, wide mouth, and built-in pouring spout make it very simple to fill with ice or your favorite cut fruits. Easy to hold, dispense, and hand wash them, yet a small enough pitcher to fit perfectly in refrigerator door."
   },
   {
     id: 3,
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
+    name: "Salt and Pepper Grinder ",
+    price: 19.99,
+    description:
+      "The tightly sealed stainless steel lid also reduces moisture entering your salt or peppercorn & guarantees that no dust, residue or other unwanted particles and ensure a positive culinary cooking experience. Please Note Salt and Peppercorns not included"
+  },
+  {
+    id: 4,
+    name: "Salt and Pepper Grinder ",
+    price: 19.99,
+    description:
+      "The tightly sealed stainless steel lid also reduces moisture entering your salt or peppercorn & guarantees that no dust, residue or other unwanted particles and ensure a positive culinary cooking experience. Please Note Salt and Peppercorns not included"
   }
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
