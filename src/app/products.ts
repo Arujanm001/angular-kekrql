cf81e0fe75a6d4603dd9b4866e977176bbf08c09
Arujanm001 <arujanm001@gmail.com>
2021-02-24 08:58:56 +0500

    2 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -4,7 +4,7 @@ export const products = [
     rate:'4.4 out of 5',
     img: "https://m.media-amazon.com/images/I/713VYm+wuGL._AC_UL320_.jpg",
     name: "Supreme Grind",
-    price: 59.95,
+    price: '59.95 $',
     description:
       "Removable grind chamber holds enough ground coffee for 32 cups.4 to 18-cup slide dial.Scoop/Cleaning Brush is included.18-position grind selector; 4- to 18-cup slide dial"
   },
@@ -13,18 +13,18 @@ export const products = [
     rate:'4.4 out of 5',
     img: "https://m.media-amazon.com/images/I/71VG6NaOt9L._AC_UL320_.jpg",
     name: "Seal Glass Pitcher",
-    price: 16.99,
+    price: '16.99$',
     description:
-      "Textured glass allows for an immediate view of the beverage contained. Made in Italy.An integrated strong handle, wide mouth, and built-in pouring spout make it very simple to fill with ice or your favorite cut fruits. Easy to hold, dispense, and hand wash them, yet a small enough pitcher to fit perfectly in refrigerator door."
+      "Textured glass allows for an immediate view of the beverage contained. Made in Italy.An integrated strong handle, wide mouth, and built-in pouring spout make it very simple to fill with ice or your favorite cut fruits. "
   },
   {
     id: 3,
     rate:'4.3 out of 5',
     img: "https://m.media-amazon.com/images/I/711s1+VosEL._AC_UL320_.jpg",
     name: "Salt and Pepper Grinder ",
-    price: 19.99,
+    price: '19.99$',
     description:
-      "The tightly sealed stainless steel lid also reduces moisture entering your salt or peppercorn & guarantees that no dust, residue or other unwanted particles and ensure a positive culinary cooking experience. Please Note Salt and Peppercorns not included"
+      "The tightly sealed stainless steel lid also reduces moisture entering your salt or peppercorn & guarantees that no dust, residue or other unwanted particles and ensure a positive culinary cooking experience. "
   },
   {
     id: 4,
@@ -32,9 +32,9 @@ export const products = [
     img:
       "https://images-na.ssl-images-amazon.com/images/I/715VnC4na-L._AC_UL270_SR270,270_.jpg",
     name: "Infuser Pitcher",
-    price: 19.99,
+    price: '19.99$',
     description:
-      "The tightly sealed stainless steel lid also reduces moisture entering your salt or peppercorn & guarantees that no dust, residue or other unwanted particles and ensure a positive culinary cooking experience. Please Note Salt and Peppercorns not included"
+      "The tightly sealed stainless steel lid also reduces moisture entering your salt or peppercorn & guarantees that no dust, residue or other unwanted particles and ensure a positive culinary cooking experience. "
   },
   {
     id: 5,
@@ -42,34 +42,34 @@ export const products = [
     img:
       "https://images-na.ssl-images-amazon.com/images/I/71AOZ3KqnEL._AC_UL270_SR270,270_.jpg",
     name: " Fox Ceramic Water Pitcher ",
-    price: 22.99,
+    price: '22.99$',
     description:
-      "Deep and big enough for milk, water, lemon water, tea, fruit juice, soft drink, storage something or used as a vase or other decorations at home.Dishwasher safe, microwave safe and freezer safe. search GoldenPlayer with product name to find more unique kitchen products."
+      "Deep and big enough for milk, water, lemon water, tea, fruit juice, soft drink, storage something or used as a vase or other decorations at home.Dishwasher safe, microwave safe and freezer safe. "
   },
   {
     id: 6,
     rate:'4.8 out of 5',
     img: "https://m.media-amazon.com/images/I/61-UI9xzQ+L._AC_UL320_.jpg",
     name: " Digital Kitchen Scale Weight ",
-    price: 27.99,
+    price: '27.99$',
     description:
-      "The digital food scale with built-in four high-precision load sensors, quick and accurate 0.1oz/1g increments when adding ingredients, measuring range: 0.1oz to 22 lbs (2g/3g to 10kg).Convenient tare button for tare calculation allows you to subtract the weight of plate or container, making it easy to achieve accurate readouts of ingredients."
+      "The digital food scale with built-in four high-precision load sensors, quick and accurate 0.1oz/1g increments when adding ingredients, measuring range: 0.1oz to 22 lbs (2g/3g to 10kg)."
   },
   {
     id: 7,
     rate:'4.6 out of 5',
     img: "https://m.media-amazon.com/images/I/71sSsrY7PUL._AC_UL320_.jpg",
     name: " Pasta Press ",
-    price: 179.0,
+    price: '179.0$',
     description:
-      "6 Quick Change Discs for spaghetti, bucatini, rigatoni, fusilli, large or small macaroni.The pasta press and all its parts are neatly organized inside its own storage case for easy access every tim.Includes Pasta press, Pusher, Pasta discs, Cleaning too"
+      "6 Quick Change Discs for spaghetti, bucatini, rigatoni, fusilli, large or small macaroni.The pasta press and all its parts are neatly organized inside its own storage case for easy access every tim."
   },
   {
     id: 8,
     rate:'4.7 out of 5',
     img: "https://m.media-amazon.com/images/I/71-wIBkLQDL._AC_UL320_.jpg",
     name: " Wine Decanter Carafe",
-    price: 18.95,
+    price: '18.95$',
     description:
       "From insulated stainless steel water bottles to beverage makers and pitchers, as well as a variety of product accessories, Takeya USA products help you stay cool, refreshed and hydrated for an active, on-the-go lifestyle."
   },
@@ -78,18 +78,18 @@ export const products = [
     rate:'4.5 out of 5',
     img: "https://m.media-amazon.com/images/I/81NC9Lx5D4L._AC_UL320_.jpg",
     name: " Plastic Food Storage ",
-    price: 21.2,
+    price: '21.2$',
     description:
-      "6-piece pantry container set is 100% airtight, shatter-resistant, and stackable to keep dry foods like flour, sugar, cereal, oats, nuts, rice, snacks, coffee, and even pet food fresh and organized in your pantry.Includes three 16 cup containers, plus lids, for a total of 6 pieces."
+      "6-piece pantry container set is 100% airtight, shatter-resistant, and stackable to keep dry foods like flour, sugar, cereal, oats, nuts, rice, snacks, coffee, and even pet food fresh and organized in your pantry."
   },
   {
     id: 10,
     rate:'4.5 out of 5',
     img: "https://m.media-amazon.com/images/I/71zE7DHhWVL._AC_UL320_.jpg",
     name: " Durable Ceramic Dinner ",
-    price: 21.2,
+    price: '54.99$',
     description:
-      "2 Piece 10.2 inch dinner plates, 2 Piece 8.27 inch dessert plates, 2 Piece soup/salad bowls, 2 Piece 400ml mugs . Whether you’re serving lunch, dinner, breakfast, this dinner sets will fit all your needs. It is elegant design adds a special visual characteristic to it. Each style has its own unique features. "
+      "2 Piece 10.2 inch dinner plates, 2 Piece 8.27 inch dessert plates, 2 Piece soup/salad bowls, 2 Piece 400ml mugs .  "
   }
 ];
 
